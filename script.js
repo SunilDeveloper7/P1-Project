05d7ba778a0a45892045a5667122224e45f8aafb
Sunil lama <Sunnil474@gmail.com>
2020-12-28 15:03:33 -0800

    Added keydownhandler

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+function keydownHandler(key) {
+    
+    switch (key) {
+        case ("KeyD"):
+            console.log('move right')
+            break
+        case ("KeyW"):
+            console.log('move up')
+            break
+        case ("KeyA"):
+            console.log('move left')
+            break
+        case ("KeyS"):
+            console.log('move down')
+            break
+
+    }
+
+}
+
+
+
 document.addEventListener('DOMContentLoaded',function() {
+    document.addEventListener('keydown',function(event) {
+
+        keydownHandler(event.code)
+
+    })
     console.log('its working')
 })
+ 
