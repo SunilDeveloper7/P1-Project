06d0a0d6723694a7200672ed7a6c196f8c2eb6f0
Sunil lama <Sunnil474@gmail.com>
2021-01-04 10:34:55 -0800

    Added 2 more canvas with its section id for 2 more display for another player and for results. Also added alert  box for game over message

--- script.js
+++ script.js
@@ -1,114 +1,143 @@
 const canvas = document.getElementById('canvas');
-//Allow us to do many things
 const ctx = canvas.getContext('2d');
-//creating box for my snake
 const box = 25;
-//both for width and height
 const canvasSize = 23;
+
+
+
 //score variable
 let score = 0;
+
+
 //load snake starting position
-let snake =[];
-snake[0] =
+let snake = [];
+
+snake[0] = 
 {
-    x:Math.floor((canvasSize/2)) * box,
-    y:Math.floor((canvasSize/2)) * box
-}
-// Set direction with my arrow key
+	x: Math.floor((canvasSize/2)) * box,
+	y: Math.floor((canvasSize/2)) * box
+};
+
+//set direction getting pressed by arrow keys
 let dir;
-let pressed = false;
+var pressed = false;
 document.addEventListener('keydown', direction);
 
-function direction(event) {
-    if(event.keyCode==37)
-        dir ='LEFT';
-    if(event.keyCode==38)
-        dir ='UP';
-    if(event.keyCode==39)
-        dir ='RIGHT';
-    if(event.keyCode==40)
-        dir ='DOWN';
+
+
+
+function direction(event){
+
+	
+if(event.keyCode == 37 && dir != 'RIGHT'){
+    dir = "LEFT";
+		
 }
-//set the location of our food
+else if(event.keyCode == 38 && dir != 'DOWN'){
+	dir = "UP";
+}
+else if(event.keyCode == 39 && dir != 'LEFT'){
+	dir = "RIGHT";
+		
+}
+else if(event.keyCode == 40 && dir != 'UP'){
+dir = "DOWN";
+		
+}
+
+}
+// set the location of our food
 let food = {
-    x:Math.floor(1 + (Math.random() * (canvasSize - 1))) * box,
-    y:Math.floor(1 + (Math.random() * (canvasSize - 1))) * box
+x: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box,
+y: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box 
 }
 //draw function
-function draw() {
+function draw(){
 //draw the background
-ctx.fillStyle = "lightgreen";
-//(x,y,width,height)
-ctx.fillRect(box,box,canvasSize*box - box,canvasSize*box - box);
-//draw snake head
-for(let i = 0; i<snake.length; i++)
+ctx.fillStyle = 'lightgreen';
+ctx.fillRect(box, box, canvasSize*box - box, canvasSize*box-box);	
+//draw the snake head and tail
+for(let i = 0; i < snake.length; i++)
 {
-    ctx.fillStyle ='green';
-    ctx.fillRect(snake[i].x,snake[i].y,box,box);
-    if(snake[i].x == food.x && snake[i].y ==food.y)
-    {
-    food = {
+	ctx.fillStyle = 'green';
+	ctx.fillRect(snake[i].x, snake[i].y, box, box);
+		if(snake[i].x == food.x && snake[i].y == food.y)
+		{
+		food = {
 			x: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box,
 			y: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box
-
-    }
+		}
 }
-    }
+	}
 
-//move the snake head
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
+	//move the snake head
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
 
-    if(dir == 'LEFT')
-        snakeX -= box;
-    if(dir == 'RIGHT')
-        snakeX += box;
-    if(dir == 'UP')
-        snakeY -= box;
-    if(dir == 'DOWN')
-        snakeY += box;
+	if(dir == "LEFT")
+	 snakeX -= box;
+	if(dir == "RIGHT")
+	 snakeX += box;
+	if(dir == "UP")
+	 snakeY -= box;
+	if(dir == "DOWN")
+	 snakeY += box;
 
-    //if my snake eat fruits 
-    if(snakeX == food.x && snakeY == food.y)
-    {
-        score +=1;
-        food = {
-            x:Math.floor(1 + (Math.random() * (canvasSize - 1))) * box,
-            y:Math.floor(1 + (Math.random() * (canvasSize - 1))) * box 
-        }
-    }
-        else
-        {
-            snake.pop();
-        }
-        let newHead = {
-            x: snakeX,
-            y: snakeY
 
-    };
-    
+	// if the snake eats the food
+	if(snakeX == food.x && snakeY == food.y)
+	{
+		score+=1;
+		food = {
+			x: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box,
+			y: Math.floor(1 + (Math.random() * (canvasSize - 1))) * box 
+		}
+	}
+	else
+	{
+		snake.pop();
+	}
 
-    //Check collision
-    function collision(head,array){
-        for(let i=0; i <array.length; i++)
-        {
-            if(head.x == array[i].x && head.y == array[i].y)
-            {
-                return true;
-            }
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	};
 
-        }
-        return false;
-    }
-    //Game over
-    if(snakeX < box || snakeY < box || snakeX >((canvasSize - 1) * box)|| snakeY >((canvasSize - 1)* box) || collision(newHead,snake))
-    {
-    clearInterval(game);
-    }
-    snake.unshift(newHead);
-    //draw in food
-    ctx.fillStyle ='red';
-    ctx.fillRect(food.x, food.y,box,box);
-    
+	
+
+
+	//check collision
+	function collision(head, array){
+		for(let i = 0; i < array.length; i++)
+		{
+			if(head.x == array[i].x && head.y == array[i].y)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	//game over
+	if(snakeX < box || snakeY < box || 
+		snakeX > ((canvasSize - 1) * box)|| snakeY > ((canvasSize - 1) * box) ||
+		collision(newHead,snake))
+	{
+        alert('Game Over');
+		clearInterval(game);
+	}
+
+	snake.unshift(newHead);
+	//draw in food
+	ctx.fillStyle = 'red';
+	ctx.fillRect(food.x, food.y, box, box);
+
+	//draw score
+	ctx.fillStyle = 'blue';
+	ctx.font = '24px Changa one';
+	ctx.clearRect(0, 0, 50, 25);
+	ctx.fillText(score, box, 0.8 * box);
+
+
 }
-let game = setInterval(draw,100);
+
+let game = setInterval(draw, 100);
